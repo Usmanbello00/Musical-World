bcb26ef42810ec905e57ba4532e2fd377bf9213b
Usmanbello00 <108652858+Usmanbello00@users.noreply.github.com>
2024-12-04 08:35:39 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('musical.mysql.database.azure.com','value','Password@123','musical_world');
+	$conn = mysqli_connect('musicals.mysql.database.azure.com','value','Password@123','musical_world');
 
 	if(!$conn)
 		die("Error while connecting...!").mysqli_connect_error($conn);
